ec1cf42f33123bc4a0a7a861bd95dbcc14329513
Lucas Augusto de Morais <lucasm310@gmail.com>
2021-08-23 19:18:45 -0300

    add handler for CORS

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_rest_api" "api_linpe" {
-  name = "api-linpe-${terraform.workspace}"
-  tags = merge(var.tags, { enviroment = terraform.workspace })
+  name               = "api-linpe-${terraform.workspace}"
+  tags               = merge(var.tags, { enviroment = terraform.workspace })
   binary_media_types = ["multipart/form-data"]
 }
 
@@ -32,4 +32,49 @@ resource "aws_api_gateway_deployment" "apideploy" {
   ]
   rest_api_id = aws_api_gateway_rest_api.api_linpe.id
   stage_name  = terraform.workspace
+}
+
+#enable cors
+resource "aws_api_gateway_method" "cors" {
+  rest_api_id   = aws_api_gateway_rest_api.api_linpe.id
+  resource_id   = aws_api_gateway_resource.proxy.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors" {
+  rest_api_id = aws_api_gateway_rest_api.api_linpe.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.cors.http_method
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_method_response" "cors" {
+  depends_on  = [aws_api_gateway_method.cors]
+  rest_api_id = aws_api_gateway_rest_api.api_linpe.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.cors.http_method
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+  response_models = {
+    "application/json"    = "Empty",
+    "multipart/form-data" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors" {
+  depends_on  = [aws_api_gateway_integration.cors, aws_api_gateway_method_response.cors]
+  rest_api_id = aws_api_gateway_rest_api.api_linpe.id
+  resource_id = aws_api_gateway_resource.proxy.id
+  http_method = aws_api_gateway_method.cors.http_method
+  status_code = 200
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
+    "method.response.header.Access-Control-Allow-Headers" = "'*'",
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
+  }
 }
